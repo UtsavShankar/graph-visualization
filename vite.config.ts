023eee026e95f6d78fb80c71e57de1963b48d27f
Utsav Shankar <utsaveshan0206@gmail.com>
2025-09-01 15:06:17 +0800

    GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const base = "https://utsavshankar.github.io/graph-visualization/"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base,
 })
