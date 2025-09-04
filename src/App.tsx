2f8a41df5b6d0d74c82ac3d3e775638cf4e816aa
Utsav Shankar <utsaveshan0206@gmail.com>
2025-09-04 11:50:47 +0800

    only load from json

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ type NodeData = { id: string; title: string; author?: string; year?: number; tag
 type EdgeData = { id?: string; source: string; target: string; relation?: string; weight?: number };
 type GraphData = { nodes: NodeData[]; edges: EdgeData[] };
 
-const STORAGE_KEY = 'bookGraph:v4';
+const STORAGE_KEY = 'bookGraph:v4:123';
 
 function useGraph() {
   const [graph, setGraph] = React.useState<GraphData>(() => {
